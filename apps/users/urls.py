fc3e040a4e7f71cb56b12616bd3bd1143fa9ad65
Eduardo López <eduardorostran@yahoo.com>
2021-12-06 10:54:39 -0600

    🚧 - working in index.html view 🚧 - working on the bd models

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
+from django.utils.functional import new_method_proxy
 from . import views
 
 urlpatterns = [
     path("", views.login, name="login"),
     path("logout/",  LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name="logout"), #necesita cmabiarse a la pagina que se redirija - mirar lo del redirect
     path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile")
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+from django.http.request import QueryDict
 from django.shortcuts import render, redirect
 from django.contrib.auth import *
 from django.contrib.auth.models import User
@@ -32,7 +33,6 @@ def login(request):
             return redirect('login')
 
 def logout(request):
-    
     if request.method=="POST":
         logout(request)
     
@@ -60,3 +60,8 @@ def register(request):
 
         print("POST method")
         return redirect("login")
+
+def profile(request):
+    if request.method == "GET":
+        queryset = {} #aqui deberia cambiar los datos del usuario
+        return render(request, 'users/profile.html', queryset)
